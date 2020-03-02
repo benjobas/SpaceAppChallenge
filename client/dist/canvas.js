742381b71df32e433c7c1d5f0513bf4e659aca3f
Bryce Balbon <bbalbon@gmail.com>
2020-03-01 22:30:13 -0800

    Add background canvas

---
+++ client/dist/canvas.js
@@ -0,0 +1,51 @@
+let totalWidth = window.innerWidth;
+let totalHeight = window.innerHeight;
+
+let canvas = document.getElementById("canvas");
+canvas.width = totalWidth;
+canvas.height = totalHeight;
+
+const c = canvas.getContext("2d");
+let stars = [];
+
+const draw = (star) => {
+  if (star.alpha <= 0 || star.x <= 0 || star.x > totalWidth || star.y <= 0 || star.y > totalHeight) {
+    createStar(star);
+  }
+  star.y -= star.velocity;
+  star.alpha -= 5e-4;
+  c.beginPath();
+  c.arc(star.x, star.y, star.scale * 3, 0, 2 * Math.PI, false);
+  if (parseInt(Math.random()) * 100 % 3 == 0)
+      c.fillStyle = "rgba(255,255,255," + star.alpha + ")";
+  else
+      c.fillStyle = "rgba(237,194,123," + star.alpha + ")";
+  c.fill()
+}
+
+function createStar(existingStar) {
+  const star = existingStar || {};
+  star.x = Math.random() * totalWidth;
+  star.y = Math.random() * totalHeight;
+  star.alpha = .2 + Math.random();
+  star.scale = .1 + Math.random() / 10;
+  star.velocity = Math.random() * .02;
+
+  return star;
+}
+
+function animate() {
+  c.clearRect(0, 0, totalWidth, totalHeight);
+  for (var n in stars) {
+      draw(stars[n]);
+  }
+
+  requestAnimationFrame(animate);
+}
+
+for (let i = 0; i < totalWidth * .2; i++) {
+    let a = createStar();
+    stars.push(a);
+}
+
+animate();
--- client/src/components/Planets.jsx
+++ client/src/components/Planets.jsx
@@ -3,14 +3,12 @@ import React, { useState, useEffect } from 'react';
 const Planets = (props) => {
   return (
     <div id="planets">
-      <svg>
-        {props.planets.map((planet, i) => (
-          <g className="circle" key={i} transform={`translate(${planet.ra * 4}, ${planet.dec < 0 ? planet.dec * 2 + 90 * 2 : planet.dec * 2})`}>
-            <circle cx="0" cy="0" r="12.875" style={{fill: "rgb(137, 200, 244)"}}>
-            </circle>
-          </g>
-        ))}
-      </svg>
+      {props.planets.map((planet, i) => (
+        <svg className="circle" key={i}>
+          <circle cx={`${planet.ra}`} cy={planet.dec < 0 ? `${planet.dec + 90}` : `${planet.dec}`} r="12.875" style={{fill: "rgb(137, 200, 244)"}}>
+          </circle>
+        </svg>
+      ))}
     </div>
   )
 }
